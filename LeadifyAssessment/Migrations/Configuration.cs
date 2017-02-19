ca58cd9bb1e79b309501b8103eaeb27f2229f280
lintoncaldecott <lcaldecott@gmail.com>
2017-02-19 08:05:15 +0200

    Minor changes to validation and sundry
    changes to model validations and a few other changes on code first
    migrations.

--- LeadifyAssessment/Migrations/Configuration.cs
+++ LeadifyAssessment/Migrations/Configuration.cs
@@ -1,10 +1,8 @@
 namespace LeadifyAssessment.Migrations
 {
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
-    using LeadifyAssessment.DataLayer.Models;
+    using DataLayer.Models;
     internal sealed class Configuration : DbMigrationsConfiguration<LeadifyAssessmentContext>
     {
         public Configuration()
@@ -14,13 +12,20 @@ namespace LeadifyAssessment.Migrations
 
         protected override void Seed(LeadifyAssessmentContext context)
         {
-            context.ContactModels.AddOrUpdate(
-              p => p.Email, 
-              new ContactModel { FirstName = "Paul", LastName= "Atreides", CellPhone=274589654, Email= "Paul@Atreides.com" },
-              new ContactModel { FirstName = "Duncan", LastName = "Idaho", CellPhone = 274589655, Email = "Duncan@Idaho.com" },
-              new ContactModel { FirstName = "Piter", LastName = "De Vries", CellPhone = 274589656, Email = "Piter@DeVries.com" }
-            );
-
+            try
+            {
+                context.ContactModels.AddOrUpdate(
+                  p => p.Email,
+                  new ContactModel { FirstName = "Paul", LastName = "Atreides", CellPhone = "0825241234", Email = "Paul@Atreides.com" },
+                  new ContactModel { FirstName = "Duncan", LastName = "Idaho", CellPhone = "0825241234", Email = "Duncan@Idaho.com" },
+                  new ContactModel { FirstName = "Piter", LastName = "De Vries", CellPhone = "0825241234", Email = "Piter@DeVries.com" }
+                );
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                throw;
+            }
         }
     }
 }
